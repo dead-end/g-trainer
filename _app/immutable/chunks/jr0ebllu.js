55055d3bfdcdfc95ed130aa12eb6c1c302dd5f61
Dead End <dead.end@inbox.ru>
2025-01-18 11:06:19 +0100

    Updates

---
+++ _app/immutable/chunks/jr0ebllu.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_1io5mqg)==null?void 0:s.base)??"/g-trainer";var a;const t=((a=globalThis.__sveltekit_1io5mqg)==null?void 0:a.assets)??e;export{t as a,e as b};
--- _app/immutable/entry/start.BwGXw8MY.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/Df73GhoG.js";export{t as start};
